2291abb03d31dab160b1cad78996a21cf4fca301
carmenjlr <carmenjlr@gmail.com>
2026-03-12 16:50:42 +0100

    Add files via upload

---
+++ entrenamiento/src/main/java/org/example/dia1_9_3_26/correccion/Telefono404casa.java
@@ -0,0 +1,22 @@
+package org.example.dia1_9_3_26.correccion;
+
+import java.util.Scanner;
+
+public class Telefono404casa {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()){
+
+            String telefono = sc.nextLine();
+
+
+
+
+        }
+
+    }
+
+}
---
+++ entrenamiento/src/main/java/org/example/dia1_9_3_26/sesion/loteria408.java
@@ -0,0 +1,39 @@
+package org.example.dia1_9_3_26.sesion;
+
+import java.util.Scanner;
+
+public class loteria408 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int cantidadDinero = sc.nextInt();
+        int limiteDia =  sc.nextInt();
+        int limiteSem = sc.nextInt();
+        int limiteMes = sc.nextInt();
+
+        int cantidadDia = 0;
+        int cantidadSem = 0;
+        int cantidadMes = 0;
+
+        while (cantidadDinero >= limiteDia) {
+            cantidadDinero -= limiteDia;
+            if (cantidadDia <= limiteSem){
+                cantidadDia++;
+            }
+
+            if (cantidadDia == limiteSem) {
+                cantidadDinero -= limiteSem;
+                cantidadSem++;
+
+            } else if (cantidadSem >= 4) {
+                cantidadDinero -= limiteMes;
+                cantidadMes++;
+            }
+
+        }
+        System.out.println(cantidadMes + " " + cantidadSem + " " + cantidadDia);
+
+    }
+}
