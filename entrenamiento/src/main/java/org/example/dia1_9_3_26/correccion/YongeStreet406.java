7e2e0a7741f01bb2d979455e56f83825d29db133
Carmen <carmenjlr@gmail.com>
2026-03-23 21:24:37 +0100

    Correccion y ejercicios

--- entrenamiento/src/main/java/org/example/dia1_9_3_26/correccion/YongeStreet406.java
+++ entrenamiento/src/main/java/org/example/dia1_9_3_26/correccion/YongeStreet406.java
@@ -1,4 +1,58 @@
 package org.example.dia1_9_3_26.correccion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class YongeStreet406 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+
+            String calle = sc.nextLine();
+            char[] resultado = new char[calle.length()];
+            Arrays.fill(resultado, '.');
+
+            List<Character> coches = new ArrayList<>();
+
+            for (int i = 0; i < calle.length(); i++) {
+                char c = calle.charAt(i);
+
+                if (c == '|') {
+                    resultado[i] = '|';
+                    int pos = i - 1;
+
+                    for (int j = coches.size() - 1; j >= 0; j--) {
+                        resultado[pos] = coches.get(j);
+                        pos = pos - 1;
+                    }
+
+                    coches.clear();
+
+                } else if (c != '.') {
+                    coches.add(c);
+                }
+            }
+
+            int numConsulta = sc.nextInt();
+
+            for (int i = 0; i < numConsulta; i++) {
+                int posicion = sc.nextInt();
+                System.out.print(resultado[posicion - 1]);
+
+                if (i < numConsulta - 1){
+                    System.out.print(" ");
+                }
+            }
+            sc.nextLine();
+
+            System.out.println();
+        }
+    }
+
+
 }
